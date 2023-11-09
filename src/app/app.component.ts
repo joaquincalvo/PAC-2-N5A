b09f8a7e5fc9b973913b17389b95dc1cc1b9a56d
joaquincalvo <joaquincalvo7@gmail.com>
2023-11-09 19:46:09 -0300

    g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { PostService } from './path-to-post-service';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-root',
